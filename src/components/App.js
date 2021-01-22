eb985a632fae1037979d2160765273a52be21418
Fernan <fernanhick@gmail.com>
2021-01-22 00:17:40 +0000

    full project commit

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,10 @@ import {AuthProvider} from '../contexts/AuthContext';
 import Signup from './Signup';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Dashboard from './Dashboard';
+import Login from './Login';
+import PrivateRoute from './PrivateRoute';
+import forgotPassword from './ForgotPassword';
+import UpdateProfile from './UpdateProfile';
 function App() {
   return (
     <Container
@@ -14,8 +18,11 @@ function App() {
         <Router>
           <AuthProvider>
             <Switch>
-              <Route exact path="/" component={Dashboard} />
+              <PrivateRoute exact path="/" component={Dashboard} />
+              <PrivateRoute path="/update-profile" component={UpdateProfile} />
               <Route path="/signup" component={Signup} />
+              <Route path="/login" component={Login} />
+              <Route path="/forgot-password" component={forgotPassword} />
             </Switch>
           </AuthProvider>
         </Router>
--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -7,10 +7,28 @@ export function useAuth() {
 export function AuthProvider({children}) {
   const [currentUser, setCurrentUser] = useState('');
   const [loading, setLoading] = useState(true);
+
   function signup(email, password) {
     return auth.createUserWithEmailAndPassword(email, password);
   }
 
+  function login(email, password) {
+    return auth.signInWithEmailAndPassword(email, password);
+  }
+
+  function logout() {
+    return auth.signOut();
+  }
+  function resetPassword(email) {
+    return auth.sendPasswordResetEmail(email);
+  }
+
+  function updateEmail(email) {
+    return currentUser.updateEmail(email);
+  }
+  function updatePassword(password) {
+    return currentUser.updatePassword(password);
+  }
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((user) => {
       setCurrentUser(user);
@@ -21,7 +39,12 @@ export function AuthProvider({children}) {
 
   const value = {
     currentUser,
+    login,
     signup,
+    logout,
+    resetPassword,
+    updateEmail,
+    updatePassword,
   };
 
   return (
